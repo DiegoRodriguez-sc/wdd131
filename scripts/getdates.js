dce29ca983f7a1e557ec0cef04fa9c6b38e10afc
Diego Rodriguez Santa Cruz <diego.rodriguez@medismart.live>
2024-04-27 14:05:27 -0300

    Add index.html, getdates.js, and base.css files

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+    let modifiedDate = new Date(document.lastModified);
+    let options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    document.getElementById('lastModified').textContent = modifiedDate.toLocaleDateString('es-ES', options);
+}
